3eca1285ce489de7d38ef9f1603a953a47598c21
Ikey Doherty <michael.i.doherty@intel.com>
2016-06-09 15:12:41 +0100

    Begin implementing the playground concept
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- tests/check-updates.c
+++ tests/check-updates.c
@@ -10,17 +10,94 @@
  */
 
 #define _GNU_SOURCE
+
+#include <assert.h>
 #include <check.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "bootman.h"
 #include "files.h"
+#include "nica/files.h"
+
+#define PLAYGROUND_ROOT TOP_BUILD_DIR "/tests/update_playground"
+
+/**
+ * Needed for intiialisation
+ */
+typedef struct PlaygroundKernel {
+        const char *version;
+        const char *ktype;
+        int release;
+        bool default_for_type;
+} PlaygroundKernel;
+
+/**
+ * Playground initialisation
+ */
+typedef struct PlaygroundConfig {
+        const char *uts_name;
+        PlaygroundKernel *initial_kernels;
+        size_t n_kernels;
+} PlaygroundConfig;
+
+/**
+ * Initialise a playground area to assert update behaviour
+ */
+static BootManager *prepare_playground(PlaygroundConfig *config)
+{
+        assert(config != NULL);
+        BootManager *m = NULL;
+
+        m = boot_manager_new();
+        if (!m) {
+                return NULL;
+        }
+
+        /* Purge last runs */
+        if (nc_file_exists(PLAYGROUND_ROOT)) {
+                if (!nc_rm_rf(PLAYGROUND_ROOT)) {
+                        fprintf(stderr, "Failed to rm_rf: %s\n", strerror(errno));
+                        return false;
+                }
+        }
+        /* Now create fresh tree */
+        if (!nc_mkdir_p(PLAYGROUND_ROOT, 00755)) {
+                fprintf(stderr, "Cannot create playground root: %s\n", strerror(errno));
+                return false;
+        }
+
+        if (!boot_manager_set_prefix(m, PLAYGROUND_ROOT)) {
+                goto fail;
+        }
+
+        /* TODO: Insert all the kernels into PLAYGROUND_ROOT */
+
+        boot_manager_set_can_mount(m, false);
+        boot_manager_set_image_mode(m, false);
+        return m;
+fail:
+        boot_manager_free(m);
+        return NULL;
+}
 
 START_TEST(bootman_image_test_simple)
 {
-        fail_if(true == true, "Test not implemented..");
+        autofree(BootManager) *m = NULL;
+
+        PlaygroundKernel init_kernels[] = {
+                { "4.6.0", "native", 180, true },
+                { "4.4.4", "native", 160, false },
+                { "4.4.0", "native", 140, false },
+        };
+        PlaygroundConfig start_conf = { "4.4.4-160.native",
+                                        init_kernels,
+                                        ARRAY_SIZE(init_kernels) };
+
+        m = prepare_playground(&start_conf);
+        fail_if(!m, "Fatal: Cannot initialise playground");
 }
 END_TEST
 
