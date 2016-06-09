f6206f13b0863aad2fe2668acc818fcecbd38002
Ikey Doherty <michael.i.doherty@intel.com>
2016-06-09 14:41:00 +0100

    Add skeleton test for updates
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

---
+++ tests/check-updates.c
@@ -0,0 +1,73 @@
+/*
+ * This file is part of clr-boot-manager.
+ *
+ * Copyright © 2016 Intel Corporation
+ *
+ * clr-boot-manager is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+#include <check.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bootman.h"
+#include "files.h"
+
+START_TEST(bootman_image_test_simple)
+{
+        fail_if(true == true, "Test not implemented..");
+}
+END_TEST
+
+static Suite *core_suite(void)
+{
+        Suite *s = NULL;
+        TCase *tc = NULL;
+
+        s = suite_create("bootman_update");
+        tc = tcase_create("bootman_update_functions");
+        tcase_add_test(tc, bootman_image_test_simple);
+        suite_add_tcase(s, tc);
+
+        return s;
+}
+
+int main(void)
+{
+        Suite *s;
+        SRunner *sr;
+        int fail;
+
+        /* syncing can be problematic during test suite runs */
+        cbm_set_sync_filesystems(false);
+
+        s = core_suite();
+        sr = srunner_create(s);
+        srunner_run_all(sr, CK_VERBOSE);
+        fail = srunner_ntests_failed(sr);
+        srunner_free(sr);
+
+        if (fail > 0) {
+                return EXIT_FAILURE;
+        }
+
+        return EXIT_SUCCESS;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
