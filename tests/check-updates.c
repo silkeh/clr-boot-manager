d54356da4e343aa4ad1543d3e47e1e0f341a7958
Ikey Doherty <michael.i.doherty@intel.com>
2016-07-12 19:00:30 +0100

    Ensure all test suites enforce verbose logging
    We want to get as much information as we can during development of CBM, so
    with this in mind, we force the log level to the lowest (DEBUG) in order to
    see all the messages we can in `make check` runs.
    
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- tests/check-updates.c
+++ tests/check-updates.c
@@ -21,6 +21,7 @@
 
 #include "bootman.h"
 #include "files.h"
+#include "log.h"
 #include "nica/files.h"
 
 #include "config.h"
@@ -285,6 +286,10 @@ int main(void)
 
         /* syncing can be problematic during test suite runs */
         cbm_set_sync_filesystems(false);
+
+        /* Ensure that logging is set up properly. */
+        setenv("CBM_DEBUG", "1", 1);
+        cbm_log_init(stderr);
 
         s = core_suite();
         sr = srunner_create(s);
