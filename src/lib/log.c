55b284881d80dd86e6a7ba7f02b57f3bc45c77a6
Ikey Doherty <michael.i.doherty@intel.com>
2016-07-12 16:19:54 +0100

    Add the new logging functionality
    These new logging functions will be used in an upcoming commit as the sole
    form of stderr messaging from clr-boot-manager. Unlike ordinary logging
    mechanisms, this should be used sparingly.
    
    It is expected that consumers of the API do not directly use the cbm_log
    function, rather they use the leveled macros, i.e. LOG_DEBUG. The default
    log level is CBM_LOG_ERROR, matching the current functionality.
    
    In all instances, any log message with a level below CBM_LOG_ERROR will not
    be emitted, *unless* it is overriden with the environmental variable CBM_DEBUG.
    This should be set to the value of the desired log level correlating with the
    CbmLogLevel enum. Thus, setting CBM_DEBUG=1 will set the minimum debug level
    to CBM_LOG_DEBUG, enabling all DEBUG and higher messages to be emitted.
    
    The main reason for this change is to enable deeper inspection of the update
    process to help triage any current and future issues, specifically during
    image generation and update automation. With this change, and the next comit
    to introduce application of the log functionality, devops will be able to
    run image builds with CBM_DEBUG set in order to create more useful log output.
    
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

---
+++ src/lib/log.c
@@ -0,0 +1,121 @@
+/*
+ * This file is part of clr-boot-manager.
+ *
+ * Copyright © 2016 Intel Corporation
+ *
+ * clr-boot-manager is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "log.h"
+#include "nica/util.h"
+
+static FILE *log_file = NULL;
+static CbmLogLevel min_log_level;
+
+#define PACKAGE_NAME_SHORT "cbm"
+
+static const char *log_str_table[] = {[CBM_LOG_DEBUG] = "DEBUG",
+                                      [CBM_LOG_INFO] = "INFO",
+                                      [CBM_LOG_SUCCESS] = "SUCCESS",
+                                      [CBM_LOG_ERROR] = "ERROR",
+                                      [CBM_LOG_FATAL] = "FATAL" };
+
+void cbm_log_init(FILE *log)
+{
+        const char *env_level = NULL;
+        log_file = log;
+
+        env_level = getenv("CBM_DEBUG");
+        if (!env_level) {
+                min_log_level = CBM_LOG_ERROR;
+                return;
+        }
+
+        if (streq(env_level, "1")) {
+                min_log_level = CBM_LOG_DEBUG;
+        } else if (streq(env_level, "2")) {
+                min_log_level = CBM_LOG_INFO;
+        } else if (streq(env_level, "3")) {
+                min_log_level = CBM_LOG_SUCCESS;
+        } else if (streq(env_level, "4")) {
+                min_log_level = CBM_LOG_ERROR;
+        } else if (streq(env_level, "5")) {
+                min_log_level = CBM_LOG_FATAL;
+        } else {
+                min_log_level = CBM_LOG_ERROR;
+        }
+}
+
+/**
+ * Ensure we're always at least initialised with stderr
+ */
+__attribute__((constructor)) static void cbm_log_first_init(void)
+{
+        cbm_log_init(stderr);
+}
+
+static inline const char *cbm_log_level_str(CbmLogLevel l)
+{
+        if (l >= 0 && l <= CBM_LOG_FATAL) {
+                return log_str_table[l];
+        }
+        return "unknown";
+}
+
+void cbm_log(CbmLogLevel level, const char *filename, int lineno, const char *format, ...)
+{
+        const char *displ = NULL;
+        va_list vargs;
+        autofree(char) *rend = NULL;
+
+        /* Respect minimum log level */
+        if (level < min_log_level) {
+                return;
+        }
+
+        displ = cbm_log_level_str(level);
+
+        va_start(vargs, format);
+
+        if (vasprintf(&rend, format, vargs) < 0) {
+                fputs("[FATAL] " PACKAGE_NAME_SHORT ": Cannot log to stream", log_file);
+                goto clean_args;
+        }
+
+        if (fprintf(log_file,
+                    "[%s] %s (%s:L%d): %s\n",
+                    displ,
+                    PACKAGE_NAME_SHORT,
+                    filename,
+                    lineno,
+                    rend) < 0) {
+                /* Forcibly fall back to stderr with the error */
+                fputs("[FATAL] " PACKAGE_NAME_SHORT ": Cannot log to stream", stderr);
+                goto clean_args;
+        }
+
+clean_args:
+        va_end(vargs);
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/lib/log.h
@@ -0,0 +1,77 @@
+/*
+ * This file is part of clr-boot-manager.
+ *
+ * Copyright © 2016 Intel Corporation
+ *
+ * clr-boot-manager is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+
+typedef enum {
+        CBM_LOG_DEBUG = 0,
+        CBM_LOG_INFO,
+        CBM_LOG_SUCCESS,
+        CBM_LOG_ERROR,
+        CBM_LOG_FATAL
+} CbmLogLevel;
+
+/**
+ * Re-initialise the logging functionality, to use a different file descriptor
+ * for logging
+ *
+ * @note This is already called once with stderr as the log file
+ */
+void cbm_log_init(FILE *log);
+
+/**
+ * Log current status/error to stderr. It is recommended to use the
+ * macros to achieve this.
+ */
+void cbm_log(CbmLogLevel level, const char *file, int line, const char *format, ...)
+    __attribute__((format(printf, 4, 5)));
+
+/**
+ * Log a simple debug message
+ */
+#define LOG_DEBUG(...) (cbm_log(CBM_LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__))
+
+/**
+ * Log an informational message
+ */
+#define LOG_INFO(...) (cbm_log(CBM_LOG_INFO, __FILE__, __LINE__, __VA_ARGS__))
+
+/**
+ * Log success
+ */
+#define LOG_SUCCESS(...) (cbm_log(CBM_LOG_SUCCESS, __FILE__, __LINE__, __VA_ARGS__))
+
+/**
+ * Log a non-fatal error
+ */
+#define LOG_ERROR(...) (cbm_log(CBM_LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__))
+
+/**
+ * Log a fatal error
+ */
+#define LOG_FATAL(...) (cbm_log(CBM_LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__))
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
