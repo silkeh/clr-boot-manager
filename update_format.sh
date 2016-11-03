3927dd99ee2c4a3d11e4154a88262de63d00f70f
Ikey Doherty <michael.i.doherty@intel.com>
2016-11-03 12:41:51 +0000

    update_format.sh: Also look for typos in the codebase
    This allows syncing recent Nica changes into the codebase as typos were
    found after the last sync, that while not critical, aren't *good*.
    
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- update_format.sh
+++ update_format.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+set -e
 clang-format -i $(find . -name '*.[ch]')
+
+# Check we have no typos.
+which misspell 2>/dev/null >/dev/null
+if [[ $? -eq 0 ]]; then
+    misspell -error `find . -name '*.[ch]'`
+fi
