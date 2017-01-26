37432cc542f1fa21655a86aa1dedca1e03067d8f
Ikey Doherty <michael.i.doherty@intel.com>
2017-01-26 17:36:54 +0000

    lib: Add a new stub to encapsulate the blkid library
    Reaching high levels of coverage within clr-boot-manager has been very
    hard due to relying heavily on native syscalls and usage of libraries for
    core functionality, i.e. blkid.
    
    Consequently we're unable to test our own behaviour in relation to managing
    disks and UUIDs, etc. This change wraps around the blkid implementation,
    allowing us to dynamically change the vtable to stub out the blkid library
    itself, and safely test without requiring root privileges.
    
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

---
+++ src/lib/blkid_stub.c
@@ -0,0 +1,169 @@
+/*
+ * This file is part of clr-boot-manager.
+ *
+ * Copyright © 2017 Intel Corporation
+ *
+ * clr-boot-manager is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include "blkid_stub.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+/**
+ * Default blkid ops vtable passes through to libblkid itself
+ */
+static CbmBlkidOps default_blkid_ops = {
+        .probe_new_from_filename = blkid_new_probe_from_filename,
+        .probe_enable_superblocks = blkid_probe_enable_superblocks,
+        .probe_set_superblocks_flags = blkid_probe_set_superblocks_flags,
+        .probe_enable_partitions = blkid_probe_enable_partitions,
+        .probe_set_partitions_flags = blkid_probe_set_partitions_flags,
+        .probe_lookup_value = blkid_probe_lookup_value,
+        .do_safeprobe = blkid_do_safeprobe,
+        .free_probe = blkid_free_probe,
+
+        /* Partition functions */
+        .probe_get_partitions = blkid_probe_get_partitions,
+        .partlist_numof_partitions = blkid_partlist_numof_partitions,
+        .partlist_get_partition = blkid_partlist_get_partition,
+        .partition_get_flags = blkid_partition_get_flags,
+        .partition_get_uuid = blkid_partition_get_uuid,
+
+        /* Misc */
+        .devno_to_wholedisk = blkid_devno_to_wholedisk,
+};
+
+/**
+ * Pointer to the currently active vtable
+ */
+static CbmBlkidOps *blkid_ops = &default_blkid_ops;
+
+void cbm_blkid_reset_vtable(void)
+{
+        blkid_ops = &default_blkid_ops;
+}
+
+void cbm_blkid_set_vtable(CbmBlkidOps *ops)
+{
+        if (!ops) {
+                cbm_blkid_reset_vtable();
+        } else {
+                blkid_ops = ops;
+        }
+        /* Ensure the vtable is valid at this point. */
+        assert(blkid_ops->probe_new_from_filename != NULL);
+        assert(blkid_ops->probe_enable_superblocks != NULL);
+        assert(blkid_ops->probe_set_superblocks_flags != NULL);
+        assert(blkid_ops->probe_enable_partitions != NULL);
+        assert(blkid_ops->probe_set_partitions_flags != NULL);
+        assert(blkid_ops->probe_lookup_value != NULL);
+        assert(blkid_ops->do_safeprobe != NULL);
+        assert(blkid_ops->free_probe != NULL);
+
+        /* partition functions */
+        assert(blkid_ops->probe_get_partitions != NULL);
+        assert(blkid_ops->partlist_numof_partitions != NULL);
+        assert(blkid_ops->partlist_get_partition != NULL);
+        assert(blkid_ops->partition_get_flags != NULL);
+        assert(blkid_ops->partition_get_uuid != NULL);
+
+        /* misc */
+        assert(blkid_ops->devno_to_wholedisk != NULL);
+}
+
+/**
+ * Probe functions
+ */
+blkid_probe cbm_blkid_new_probe_from_filename(const char *filename)
+{
+        return blkid_ops->probe_new_from_filename(filename);
+}
+
+int cbm_blkid_probe_enable_superblocks(blkid_probe pr, int enable)
+{
+        return blkid_ops->probe_enable_superblocks(pr, enable);
+}
+
+int cbm_blkid_probe_set_superblocks_flags(blkid_probe pr, int flags)
+{
+        return blkid_ops->probe_set_superblocks_flags(pr, flags);
+}
+
+int cbm_blkid_probe_enable_partitions(blkid_probe pr, int enable)
+{
+        return blkid_ops->probe_enable_partitions(pr, enable);
+}
+
+int cbm_blkid_probe_set_partitions_flags(blkid_probe pr, int flags)
+{
+        return blkid_ops->probe_set_partitions_flags(pr, flags);
+}
+
+int cbm_blkid_do_safeprobe(blkid_probe pr)
+{
+        return blkid_ops->do_safeprobe(pr);
+}
+
+int cbm_blkid_probe_lookup_value(blkid_probe pr, const char *name, const char **data, size_t *len)
+{
+        return blkid_ops->probe_lookup_value(pr, name, data, len);
+}
+
+void cbm_blkid_free_probe(blkid_probe pr)
+{
+        blkid_ops->free_probe(pr);
+}
+
+/**
+ * Partition functions
+ */
+blkid_partlist cbm_blkid_probe_get_partitions(blkid_probe pr)
+{
+        return blkid_ops->probe_get_partitions(pr);
+}
+
+int cbm_blkid_partlist_numof_partitions(blkid_partlist ls)
+{
+        return blkid_ops->partlist_numof_partitions(ls);
+}
+
+blkid_partition cbm_blkid_partlist_get_partition(blkid_partlist ls, int n)
+{
+        return blkid_ops->partlist_get_partition(ls, n);
+}
+
+unsigned long long cbm_blkid_partition_get_flags(blkid_partition par)
+{
+        return blkid_ops->partition_get_flags(par);
+}
+
+const char *cbm_blkid_partition_get_uuid(blkid_partition par)
+{
+        return blkid_ops->partition_get_uuid(par);
+}
+
+/**
+ * Misc functions
+ */
+int cbm_blkid_devno_to_wholedisk(dev_t dev, char *diskname, size_t len, dev_t *diskdevno)
+{
+        return blkid_ops->devno_to_wholedisk(dev, diskname, len, diskdevno);
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/lib/blkid_stub.h
@@ -0,0 +1,124 @@
+/*
+ * This file is part of clr-boot-manager.
+ *
+ * Copyright © 2017 Intel Corporation
+ *
+ * clr-boot-manager is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#define _GNU_SOURCE
+#include <blkid.h>
+
+/**
+ * Defines the vtable used for all blkid operations within clr-boot-manager.
+ * The default internal vtable will pass through all operations to libblkid.
+ */
+typedef struct CbmBlkidOps {
+        /* Probe functions */
+        blkid_probe (*probe_new_from_filename)(const char *filename);
+        int (*probe_enable_superblocks)(blkid_probe pr, int enable);
+        int (*probe_set_superblocks_flags)(blkid_probe pr, int flags);
+        int (*probe_enable_partitions)(blkid_probe pr, int enable);
+        int (*probe_set_partitions_flags)(blkid_probe pr, int flags);
+        int (*probe_lookup_value)(blkid_probe pr, const char *name, const char **data, size_t *len);
+        int (*do_safeprobe)(blkid_probe pr);
+        void (*free_probe)(blkid_probe pr);
+
+        /* Partition functions */
+        blkid_partlist (*probe_get_partitions)(blkid_probe pr);
+        int (*partlist_numof_partitions)(blkid_partlist ls);
+        blkid_partition (*partlist_get_partition)(blkid_partlist ls, int n);
+        unsigned long long (*partition_get_flags)(blkid_partition par);
+        const char *(*partition_get_uuid)(blkid_partition par);
+
+        /* Misc functions */
+        int (*devno_to_wholedisk)(dev_t dev, char *diskname, size_t len, dev_t *diskdevno);
+} CbmBlkidOps;
+
+/**
+ * Define an empty blkid_probe for testing
+ */
+#define CBM_BLKID_PROBE_NULL ((blkid_probe)0)
+
+/**
+ * Define a "set" blkid_probe for testing
+ */
+#define CBM_BLKID_PROBE_SET ((blkid_probe)1)
+
+/**
+ * Define an empty blkid_partlist for testing
+ */
+#define CBM_BLKID_PARTLIST_NULL ((blkid_partlist)0)
+
+/**
+ * Define a "set" blkid_partlist for testing
+ */
+#define CBM_BLKID_PARTLIST_SET ((blkid_partlist)1)
+
+/**
+ * Define an empty blkid_partition for testing
+ */
+#define CBM_BLKID_PARTITION_NULL ((blkid_partition)0)
+
+/**
+ * Define a "set" blkid_partition for testing
+ */
+#define CBM_BLKID_PARTITION_SET ((blkid_partition)1)
+
+/**
+ * Reset the blkid vtable
+ */
+void cbm_blkid_reset_vtable(void);
+
+/**
+ * Set the vfunc table used for all blkid operations within clr-boot-manager
+ *
+ * @note Passing null has the same effect as calling cbm_blkid_reset
+ * The vtable will be checked to ensure that it is valid at this point, so
+ * only call this when the vtable is fully populated.
+ */
+void cbm_blkid_set_vtable(CbmBlkidOps *ops);
+
+/**
+ * Probe related wrappers
+ */
+blkid_probe cbm_blkid_new_probe_from_filename(const char *filename);
+int cbm_blkid_probe_enable_superblocks(blkid_probe pr, int enable);
+int cbm_blkid_probe_set_superblocks_flags(blkid_probe pr, int flags);
+int cbm_blkid_probe_enable_partitions(blkid_probe pr, int enable);
+int cbm_blkid_probe_set_partitions_flags(blkid_probe pr, int flags);
+int cbm_blkid_do_safeprobe(blkid_probe pr);
+int cbm_blkid_probe_lookup_value(blkid_probe pr, const char *name, const char **data, size_t *len);
+void cbm_blkid_free_probe(blkid_probe pr);
+
+/**
+ * Partition related wrappers
+ */
+blkid_partlist cbm_blkid_probe_get_partitions(blkid_probe pr);
+int cbm_blkid_partlist_numof_partitions(blkid_partlist ls);
+blkid_partition cbm_blkid_partlist_get_partition(blkid_partlist ls, int n);
+unsigned long long cbm_blkid_partition_get_flags(blkid_partition par);
+const char *cbm_blkid_partition_get_uuid(blkid_partition par);
+
+/**
+ * Misc related wrappers
+ */
+int cbm_blkid_devno_to_wholedisk(dev_t dev, char *diskname, size_t len, dev_t *diskdevno);
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
