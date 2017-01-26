1ef4174c8c857fa88938c13b15c0454f71d08a0b
Ikey Doherty <michael.i.doherty@intel.com>
2017-01-26 16:22:58 +0000

    lib/probe: Add missing modelines
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- src/lib/probe.c
+++ src/lib/probe.c
@@ -229,3 +229,16 @@ void cbm_probe_free(CbmDeviceProbe *probe)
         free(probe);
         return;
 }
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
--- src/lib/probe.h
+++ src/lib/probe.h
@@ -35,3 +35,16 @@ CbmDeviceProbe *cbm_probe_path(const char *path);
  * Free an existing probe
  */
 void cbm_probe_free(CbmDeviceProbe *probe);
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
